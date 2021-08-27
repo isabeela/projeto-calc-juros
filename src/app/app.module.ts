338a19a3a597b1af7b8b0762d7a4be5da83e0e10
isabeela <isabeelabg@gmail.com>
2021-08-27 20:13:36 -0300

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,25 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { JurosSimplesComponent } from './juros-simples/juros-simples.component';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([{ path: '', component: HomeComponent }])
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'jurossimples', component: JurosSimplesComponent }
+    ])
   ],
-  declarations: [AppComponent, HelloComponent, NavbarComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    NavbarComponent,
+    HomeComponent,
+    JurosSimplesComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
