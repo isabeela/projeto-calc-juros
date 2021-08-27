9b034ab97546aea0a1692280f3e9957df97ce1ed
isabeela <isabeelabg@gmail.com>
2021-08-27 20:50:58 -0300

    4 files modified, 1 added

--- src/app/juroscomposto/juroscomposto.component.ts
+++ src/app/juroscomposto/juroscomposto.component.ts
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./juroscomposto.component.css']
 })
 export class JuroscompostoComponent implements OnInit {
+  capit: number;
+  taxa: number;
+  tempo: number;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.capit = 0;
+    this.taxa = 0;
+    this.tempo = 0;
   }
 
+  getCapital() {
+    return this.capit;
+  }
+
+  getTaxa() {
+    return this.taxa;
+  }
+
+  getTempo() {
+    return this.tempo;
+  }
+
+  juros(capit, taxa, tempo) {
+    return this.capit * Math.pow(1 + taxa / 100, tempo);
+  }
+
+  getCapitalList() {
+    let list: number[] = [];
+
+    for (let i = 1; i <= this.tempo; i++) list.push(this.getTempo());
+
+    return list;
+  }
+
+  ngOnInit() {}
 }
--- src/app/jurossimples/jurossimples.component.ts
+++ src/app/jurossimples/jurossimples.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./jurossimples.component.css']
 })
 export class JurossimplesComponent implements OnInit {
+  capit: 0;
+  taxa: number;
+  tempo: number;
+  result: 0;
 
-  constructor() { }
-
-  ngOnInit() {
+  getCapital() {
+    return this.capit;
   }
 
+  getTaxa() {
+    return this.taxa;
+  }
+
+  getTempo() {
+    return this.tempo;
+  }
+
+  getResult() {
+    {
+      {
+        return (this.capit * this.taxa * this.tempo) / 100;
+      }
+    }
+  }
+
+  getCapitalList() {
+    let list: number[] = [];
+
+    for (let i = 1; i <= 1; i++) list.push(this.getCapital());
+
+    return list;
+  }
+
+  constructor() {}
+
+  ngOnInit() {}
 }
